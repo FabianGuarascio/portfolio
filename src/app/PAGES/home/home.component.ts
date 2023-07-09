56c4045f6350df92d1d0d1ce83ef57f0fc6a93fb
Fabian <guarascio.fabiand@gmail.com>
2023-07-09 17:28:47 -0300

    Added a feature to allow user input to show pages

--- src/app/PAGES/home/home.component.ts
+++ src/app/PAGES/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { filter, fromEvent, tap } from 'rxjs';
 import { Card } from 'src/app/TYPES/card.model';
 @Component({
   selector: 'app-home',
@@ -8,9 +9,24 @@ import { Card } from 'src/app/TYPES/card.model';
 export class HomeComponent  {
   cardToShow: Card | null = null
 
-
-  addClassToShow(card: Card){
+  addClassToShow(card: Card | null){
     this.cardToShow = card
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeydown(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key === 'a') {
+      event.preventDefault();
+      this.cardToShow === 'about-me' ? this.addClassToShow(null) : this.addClassToShow('about-me')
+    } else if (event.ctrlKey && event.key === 'p') {
+      event.preventDefault();
+      this.cardToShow === 'projects' ? this.addClassToShow(null) : this.addClassToShow('projects')
+    } else if (event.ctrlKey && event.key === 'c') {
+      event.preventDefault();
+      this.cardToShow === 'contact' ? this.addClassToShow(null) : this.addClassToShow('contact')
+    } else if( event.key === 'Escape'){
+      this.addClassToShow(null)
+    }
   }
 
 }
